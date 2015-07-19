452c2417842292074535fbcac423a9f1d2013194
developer <mcorpucci@hotmail.com>
2015-07-19 09:36:42 -0300

    Initial project setup.

---
+++ Gestor.net/Gestor.Domain/Product/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gestor.Domain {
+    public class Product {
+        public int Id { get; set; }
+        public string SupplierProductCode { get; set; }
+        public string MainDescription { get; set; }
+        public string SecondaryDescription { get; set; }
+        public string Comments { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ Gestor.net/Gestor.Tests/Unit/Services/ProductServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Gestor.Domain;
+
+namespace Gestor.Tests {
+
+    [TestFixture]
+    public class ProductServiceTest {
+        private Product product;
+
+        [SetUp]
+        public void SetUp() {
+            product = new Product();
+            product.SupplierProductCode = "ABC123";
+            product.MainDescription = "Dummy product";
+            product.CreationDate = new DateTime(2015, 01, 01);
+            product.ModificationDate = new DateTime(2015, 01, 01);
+            product.isActive = true;
+
+        }
+
+        [Test]
+        public void SaveProduct() {
+            
+        }
+    }
+}
